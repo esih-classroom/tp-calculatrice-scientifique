4487608791661d1f9e8c6594bcac32c72858fba8
Louis Midson LAJEANTY <lmlajeanty@dinasa.ht>
2026-02-03 10:50:44 -0500

    fix: verifier chaque saisie separement

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 def main():
+    nombre1 = 0
+    nombre2 = 0
+    
+    saisie_in_correct = True
+
+    while saisie_in_correct:
+        try:
+            nombre1 = float(input("Entrer le premier nombre: "))
+            saisie_in_correct = False
+        except ValueError:
+            print("Saisie Incorrect ! Veuillez utiliser un nombre.")
 
     saisie_in_correct = True
 
     while saisie_in_correct:
         try:
-
-            nombre1 = float(input("Entrer le premier nombre: "))
             nombre2 = float(input("Entre le second nombre: "))
-
             saisie_in_correct = False
-
-            print(f"La somme est: {nombre1 + nombre2}")
         except ValueError:
-            print("Saisie Incorrect ! Veuillez utiliser des nombres .")
+            print("Saisie Incorrect ! Veuillez utiliser un nombre.")
 
+    print(f"La somme est: {nombre1 + nombre2}")
 
 if __name__ == "__main__":
     main()
