eb95cb720d1d6c20695ac4ff765d862cce3d6dc3
Louis Midson LAJEANTY <lmlajeanty@dinasa.ht>
2026-02-03 10:43:30 -0500

    fix: verifier la saisie utilisateur

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 def main():
-    nombre1 = float(input("Entrer le premier nombre: "))
-    nombre2 = float(input("Entre le second nombre: "))
 
-    print(f"La somme est: {nombre1 + nombre2}")
+    saisie_in_correct = True
+
+    while saisie_in_correct:
+        try:
+
+            nombre1 = float(input("Entrer le premier nombre: "))
+            nombre2 = float(input("Entre le second nombre: "))
+
+            saisie_in_correct = False
+
+            print(f"La somme est: {nombre1 + nombre2}")
+        except ValueError:
+            print("Saisie Incorrect ! Veuillez utiliser des nombres .")
 
 
 if __name__ == "__main__":
