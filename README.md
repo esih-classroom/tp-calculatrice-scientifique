da1328def82efc1e1657f497dc9390eb0a475a19
Louis Midson LAJEANTY <lmlajeanty@dinasa.ht>
2026-02-04 10:27:53 -0500

    Edit made by Whitchy

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Ce projet consiste a creer une calculatrice evolutive avec python
 ## Objectifs
 
 - Permettre a l'utilisateur de faire l'addition de deux nombres
+- Ajouter la possibiliter de faire d'autres operations arithmetiques
--- main.py
+++ main.py
@@ -12,10 +12,54 @@ def input_number(message: str) -> float:
 
 
 def main():
-    nombre1 = input_number("Entrer le premier nombre: ")
-    nombre2 = input_number("Entrer le second nombre : ")
-    
-    print(f"La somme est: {nombre1 + nombre2}")
+
+    continuer = True
+
+    while continuer:
+
+        print("Choisir une option: \n")
+        
+        print("1: Addition")
+        print("2: Soustraction")
+        print("3: Multiplication")
+        print("4: Division")
+        print("0: Quitter")
+
+        option = input_number(">>> ")
+
+        if option == 0:
+            exit()
+
+        if option not in [1, 2, 3, 4]:
+            print("Choix invalide.")
+            continue
+        
+
+        nombre1 = input_number("Entrer le premier nombre: ")
+        nombre2 = input_number("Entrer le second nombre : ")
+
+        resultat = 0
+
+        if option == 1:
+            resultat = nombre1 + nombre2
+        elif option == 2:
+            resultat = nombre1 - nombre2
+        elif option == 3:
+            resultat = nombre1 * nombre2
+        elif option == 4:
+            resultat = nombre1 / nombre2
+        else:
+            # exit()
+            return
+
+        print(f"Le resultat est: {resultat}")
+
+        choix = input("Voulez vous continuer (o/n) [n]: ")
+
+        if choix == 'o' or choix == 'O':
+            continuer = True
+        else:
+            continuer = False
 
 if __name__ == "__main__":
     main()
